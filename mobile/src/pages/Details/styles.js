1f736858d946463ade8591c0290db8701720fb8b
HenriqueTavaresAguiar <ricktavares76@gmail.com>
2020-03-26 18:59:46 -0300

    Página: Details

---
+++ mobile/src/pages/Details/styles.js
@@ -0,0 +1,81 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingHorizontal: 24,
+		paddingTop: Constants.statusBarHeight + 20,
+	},
+
+	header: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center'
+	},
+
+	incidentContainer: {
+		marginTop: 28,
+	},
+
+	incident: {
+		padding: 24,
+		borderRadius: 8,
+		backgroundColor: '#fff',
+		marginBottom: 16,
+	},
+
+	incidentProperty: {
+		fontSize: 13,
+		color: '#41414d',
+		fontWeight: 'bold',
+		marginTop: 18,
+	},
+
+	incidentValue: {
+		fontSize: 14,
+		marginTop: 4,
+		color: '#737380'
+	},
+
+	contactBox: {
+		padding: 24,
+		borderRadius: 8,
+		backgroundColor: '#fff',
+		marginBottom: 16,
+	},
+
+	heroTitle: {
+		fontWeight: 'bold',
+		fontSize: 20,
+		color: '#13131a',
+		lineHeight: 30,
+	},
+
+	heroDescription: {
+		fontSize: 15,
+		color: '#737380',
+		marginTop: 16
+	},
+
+	actions: {
+		marginTop: 16,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+
+	action: {
+		backgroundColor: '#e02041',
+		borderRadius: 8,
+		height: 50,
+		width: '48%',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+
+	actionText: {
+		color: '#fff',
+		fontSize: 14,
+		fontWeight: 'bold'
+	}
+});
